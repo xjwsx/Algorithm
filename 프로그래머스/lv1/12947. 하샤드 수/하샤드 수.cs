5e947473d7bb3703447a190b211b2d98b42dbcd5
xjwsx <134588087+xjwsx@users.noreply.github.com>
2023-09-07 09:35:15 +0900

    [level 1] Title: 하샤드 수, Time: 0.86 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+public class Solution 
+{
+    public bool solution(int x) 
+    {
+        bool answer = true;
+        string temp = x.ToString();
+        int sum = 0;
+        for(int i = 0; i < temp.Length; i++)
+        {
+            sum += int.Parse(temp[i].ToString());
+        }
+        if(x % sum == 0)
+        {
+            answer = true;
+        }
+        else
+        {
+            answer = false;
+        }
+        return answer;
+    }
+}
