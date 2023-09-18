292e0f94e4a64f7b4f3b6bb9621ee9d99ac35934
xjwsx <134588087+xjwsx@users.noreply.github.com>
2023-09-18 10:34:17 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.23 ms, Memory: 30.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,13 @@
+using System;
+public class Solution 
+{
+    public string solution(int n) 
+    {
+        string answer = "";
+        for(int i = 0; i < n; i++)
+        {
+            answer += (i % 2 == 0) ? '수' : '박';
+        }
+        return answer;
+    }
+}
