6f7eae011ed37fad1458059d4d53fa384375706d
xjwsx <134588087+xjwsx@users.noreply.github.com>
2023-10-12 10:03:10 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.52 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution 
+{
+    public int[] solution(string s) 
+    {
+        int[] answer = new int[s.Length];
+        Dictionary<char,int> dic = new Dictionary<char,int>();
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(!dic.ContainsKey(s[i]))
+            {
+                answer[i] = -1;
+                dic.Add(s[i], i);
+            }
+            else
+            {
+                answer[i] = i - dic[s[i]];
+                dic[s[i]] = i;
+                
+            }
+        }
+        return answer;
+    }
+}
