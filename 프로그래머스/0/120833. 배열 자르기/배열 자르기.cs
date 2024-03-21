e8a5d44f4002b1da000e1d2643be2a266276776a
jin woo sung <134588087+xjwsx@users.noreply.github.com>
2024-03-21 17:11:24 +0900

    [level 0] Title: 배열 자르기, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1 + 1] ;
+        int k = 0;
+        for(int i = num1; i <= num2; i++)
+        {
+            answer[k] += numbers[i];
+            k++;
+        }
+            
+        return answer;
+    }
+}
