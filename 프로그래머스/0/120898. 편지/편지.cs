9ff4d02d9393f9598dcc4790b1bccb3ea96d7bc4
jin woo sung <134588087+xjwsx@users.noreply.github.com>
2024-03-21 15:56:58 +0900

    [level 0] Title: 편지, Time: 0.15 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.cs
+++ 프로그래머스/0/120898. 편지/편지.cs
@@ -3,10 +3,7 @@ using System;
 public class Solution {
     public int solution(string message) {
         int answer = 0;
-        for(int i = 0; i < message.Length; i++)
-        {
-            answer++;
-        }
-        return answer * 2;
+        answer = message.Length * 2;
+        return answer;
     }
 }
